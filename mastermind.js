ccd428aa14b3f7d66e450f896652864b3df24b67
muudzo <tatendawalter62@gmail.com>
2025-03-26 20:03:09 +0100

    jop server try

--- mastermind.js
+++ mastermind.js
@@ -1,3 +1,75 @@
+const GameManager = {
+    onGameFinishClicked: function () {
+        //Send message to server
+        PusherManager.sendMessageToChannel({
+            msg: 'Game Finished!',
+            gameID: this.GAME_ID
+        });
+        //
+        document.getElementById('game').classList.remove('--show');
+        document.getElementById('result').classList.add('--show');
+    }
+};
+
+
+
+
+
+
+
+
+
+var PusherManager = {
+CHANNEL_ID: "blockbuster",
+
+pusher: null,
+bIsHost: false,
+presenceChannel: null,
+sUserID: "",
+bIsConnected: false,
+
+init: function () {
+    Pusher.logToConsole = true;
+
+    this.pusher = new Pusher('34aeee625e438241557b', {
+        cluster: 'eu',
+        forceTLS: true,
+        authEndpoint: 'https://interactionfigure.nl/nhl/blockbusterauth/pusher_auth.php'
+    });
+},
+
+connectToChannel: function () {
+    this.presenceChannel = this.pusher.subscribe('presence-'+this.CHANNEL_ID);
+    this.presenceChannel.bind('pusher:subscription_succeeded', this.onSubscriptionSucceeded.bind(this));
+},
+
+onSubscriptionSucceeded: function (_data) {
+    this.sUserID = _data.myID+"";
+
+    GameConnector.onPusherConnected()
+    
+    this.presenceChannel.bind('pusher:member_added', this.onMemberAdded.bind(this));
+    this.presenceChannel.bind('pusher:member_removed', this.onMemberRemoved.bind(this));
+    this.presenceChannel.bind('client-messagetochannel', this.onMessageFromOtherPlayer.bind(this));
+},
+
+onMemberAdded: function (_data) {
+    console.log('onMemberAdded', _data);
+},
+
+onMemberRemoved: function (_data) {
+    console.log('onMemberRemoved', _data);
+},
+
+sendMessageToChannel: function (_msg) {
+    this.presenceChannel.trigger('client-messagetochannel', _msg);
+},
+
+onMessageFromOtherPlayer: function (_msg) {
+    console.log('onMessageFromOtherPlayer', _msg);
+}
+};
+
 const secretCode = "260";
 let attempts = 0;
 let maxAttempts = 5;
